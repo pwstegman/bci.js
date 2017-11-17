dba184ad87e081c7766bb266b20c77b94c722601
pwstegman <pwstegman@gmail.com>
2017-11-17 13:22:49 -0600

    Added docs

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+/**
+ * A module for BCI design and EEG signal processing.
+ * @module webbci
+ */
+
+/** webbci.signal - Signal processing */
 exports.signal = require('./lib/signal.js');
+
+/** webbci.network - Retrieve EEG data over OSC */
 exports.network = require('./lib/network.js');
 
 var pierce = require('pierce');
+
+/** webbci.lda - Use linear discriminant analysis to classify feature vectors */
 exports.lda = pierce.lda;
+
+/** webbci.csp - Compute the common spatial pattern for given signals */
 exports.csp = pierce.csp;
