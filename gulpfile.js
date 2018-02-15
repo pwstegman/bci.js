32acb8b4439fc4e7122a9dd744e6005e6d504ac6
pwstegman <pwstegman@gmail.com>
2018-02-15 02:32:18 -0600

    dist script

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,13 @@ var fs = require('fs');
 var gulp = require('gulp');
 var mkdirp = require('mkdirp');
 
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = uglify = require('gulp-uglify-es').default;
+var sourcemaps = require('gulp-sourcemaps');
+
 gulp.task('build', function () {
 	var libdir = 'lib';
 	var libs = ['math', 'network', 'data'];
@@ -13,30 +20,38 @@ gulp.task('build', function () {
 		if (err) console.error(err)
 	});
 
-	// Compile together libraries
+	var out = fs.openSync(builddir + '/index.js', 'w');
+	fs.writeSync(out, header);
+
 	libs.forEach(lib => {
 		var path = libdir + '/' + lib;
 		var files = fs.readdirSync(path);
-		var out = fs.openSync(builddir + '/' + lib + '.js', 'w');
-
-		fs.writeSync(out, header);
 
 		files.forEach(file => {
 			var functionName = file.substring(0, file.length - 3);
 			fs.appendFileSync(out, 'module.exports.' + functionName + ' = require(\'../' + path + '/' + file + '\');\n');
 		});
-
-		fs.closeSync(out);
 	});
 
-	var index = fs.openSync(builddir + '/index.js', 'w');
-	fs.writeSync(index, header);
+	fs.closeSync(out);
+});
 
-	libs.forEach(lib => {
-		fs.appendFileSync(index, 'module.exports.' + lib + ' = require(\'./' + lib + '.js\');\n');
-	});
-
-	fs.appendFileSync(index, 'module.exports.mathjs = require(\'mathjs\');');
-
-	fs.closeSync(index);
+gulp.task('dist', function () {
+	return browserify({
+		debug: true,
+		entries: './index.js',
+		standalone: 'bci',
+		ignore: [require.resolve('mathjs')]
+	})
+		.transform(babelify.configure({
+			presets: ["env"]
+		}))
+		.bundle()
+		.on('error', (...args) => console.log(args))
+		.pipe(source('bci.minnm.js'))
+		.pipe(buffer())
+		.pipe(sourcemaps.init({ loadMaps: true }))
+		.pipe(uglify())
+		.pipe(sourcemaps.write('./maps', { addComment: false }))
+		.pipe(gulp.dest('dist'))
 });
