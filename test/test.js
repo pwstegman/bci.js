2028e6f0cca50b4e7ffe4345341a58386f1d0d2d
Pierce Stegman <pwstegman@gmail.com>
2018-04-22 02:11:57 -0500

    Updated for new psd method

--- test/test.js
+++ test/test.js
@@ -232,6 +232,15 @@ describe('math', function(){
 			var bandPower = bci.signalBandPower([1, 2, 3, 4, 5, 6, 7], 256, 'alpha', 8);
 			var expected = 19.2263;
 			assert(Math.abs(bandPower - expected) < 0.0001);
+
+			bandPower = bci.signalBandPower([1, 2, 3, 4, 5, 6, 7], 256, 'alpha');
+			expected = 19.2263;
+			assert(Math.abs(bandPower - expected) < 0.0001);
+
+			var psd = bci.psd([1, 2, 3, 4, 5, 6, 7]);
+			bandPower = bci.psdBandPower(psd, 256, 'alpha');
+			expected = 19.2263;
+			assert(Math.abs(bandPower - expected) < 0.0001);
 		});
 	});
 
