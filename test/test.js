1b32803837e700c54a0f42389994d9b0c654812e
Pierce Stegman <pwstegman@gmail.com>
2018-04-22 01:59:34 -0500

    Updated psd

--- test/test.js
+++ test/test.js
@@ -216,8 +216,14 @@ describe('math', function(){
 
 	describe('psd', function(){
 		it('Computes the power spectral density of a signal', function(){
-			var psd = bci.psd([1,2,1,2,5,2,1,2], 8);
+			var psd = bci.psd([1,2,1,2,5,2,1,2], {truncate: true});
 			assert(arrayAlmostEqual(psd, [16, 4, 4, 4]));
+
+			psd = bci.psd([1,2,3,4], {fftSize: 2});
+			assert(arrayAlmostEqual(psd, [3, 1]));
+
+			psd = bci.psd([1,2,3,4], {fftSize: 2, truncate: true});
+			assert(arrayAlmostEqual(psd, [3]));
 		});
 	});
 
