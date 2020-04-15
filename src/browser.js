230e175802041e9900db03358c934a455ae3be1e
Pierce Stegman <pwstegman@gmail.com>
2020-04-15 16:27:02 -0400

    Removed unused dependency: csv

--- src/browser.js
+++ src/browser.js
@@ -1,5 +1,5 @@
 // This file was auto generated, changes will be overwritten
-// Created on Wed Apr 15 2020 16:18:26 GMT-0400 (Eastern Daylight Time)
+// Created on Wed Apr 15 2020 16:20:48 GMT-0400 (Eastern Daylight Time)
 // This module excludes Node.js specific methods so it can be used in the browser
 /** @module bcijs */
 export * from './compat/averageBandPowers';
